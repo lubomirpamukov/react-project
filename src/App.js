482d5a628159a4192ff39eeba63abfd586eb1ffa
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-24 15:22:24 +0300

    added dropdown and accordion page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { useContext } from "react";
-import NavigationContext from "./components/context/NavigationContext";
+import Route from "./components/Route";
+import DropdownPage from "./pages/DropdownPage";
+import AccordionPage from "./pages/AccordionPage";
+import Link from "./components/Link";
 
-function App(){
-   return <div></div>
+function App() {
+  return (
+    <div>
+      <Link to={"/dropdown"}>Dropdown</Link>
+      <Link to={"/accordion"}>Accordion</Link>
+      <div>
+        <Route path={"/dropdown"} children={<DropdownPage />} />
+        <Route path={"/accordion"} children={<AccordionPage />} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
