8aad3ec9da749182ec48f73bfb94403fce5a5c2f
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-25 08:39:33 +0300

    added modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import Route from "./components/Route";
 import DropdownPage from "./pages/DropdownPage";
 import AccordionPage from "./pages/AccordionPage";
-import Link from "./components/Link";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from "./components/Sidebar";
+import ModalPage from "./pages/ModalPage";
 
 function App() {
   return (
-    <div>
-      <Link to={"/dropdown"}>Dropdown</Link>
-      <Link to={"/accordion"}>Accordion</Link>
-      <div>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
+      <div className="col-span-5">
         <Route path={"/dropdown"} children={<DropdownPage />} />
         <Route path={"/accordion"} children={<AccordionPage />} />
+        <Route path={"/buttons"} children={<ButtonPage />} />
+        <Route path={"/modal"} children={<ModalPage />} />
       </div>
     </div>
   );
