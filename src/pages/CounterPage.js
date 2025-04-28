8efdc51c09d9c185d34a6dd340e2e96ac7a4c8b9
Lubomir Pamukov <lubomir.pamukov@gmail.com>
2025-04-28 12:22:13 +0300

    refactor counter componenet to use immer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,71 +1,64 @@
+import { produce } from "immer";
 import Button from "../components/buttons/Button2";
 import { useReducer } from "react";
 import Panel from "../components/Panel";
 
-const INCREMENT_COUNT = 'increment';
-const DECREMENT_COUNT = 'decrement';
-const SET_VALUE_TO_ADD = 'set-value-to-add';
-const ADD_VALUE = 'add-value';
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "set-value-to-add";
+const ADD_VALUE = "add-value";
 
-function reducer(state,action){
-
-  switch(action.type){
+function reducer(state, action) {
+  switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1
-      };
+      state.count = state.count + 1;
+      return;
     case DECREMENT_COUNT:
-      return{
-        ...state,
-        count: state.count - 1
-      };
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return{
-        ...state,
-        value: state.value = action.payload
-      };
+      state.value = action.payload;
+      return;
     case ADD_VALUE:
-      return{
-        count: state.count + state.value,
-        value: 0
-      }
+      state.count += state.value;
+      state.value = 0;
+      return;
     default:
-      throw new Error("invalid action type: " + action.type)
+      throw new Error("invalid action type: " + action.type);
   }
 }
 
 function CounterPage({ initialCount }) {
-  const [state, dispatch] = useReducer(reducer,{
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: 0,
-    value: 0
-  })
+    value: 0,
+  });
 
   const increment = () => {
     dispatch({
-      type: INCREMENT_COUNT
-    })
+      type: INCREMENT_COUNT,
+    });
   };
 
   const decrement = () => {
     dispatch({
-      type: DECREMENT_COUNT
-    })
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (event) => {
     dispatch({
       type: SET_VALUE_TO_ADD,
-      payload: Number(event.target.value)
-    })
+      payload: Number(event.target.value),
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     dispatch({
       type: ADD_VALUE,
-    })
-  }
+    });
+  };
 
   return (
     <Panel className="relative">
@@ -84,7 +77,6 @@ function CounterPage({ initialCount }) {
       <form onSubmit={() => handleSubmit(event)}>
         <label htmlFor="add-a-lot">
           <input
-
             className="p-1 mt-3 mb-3 bg-gray-100 rounded-lg border"
             id="add-a-lot"
             type="number"
